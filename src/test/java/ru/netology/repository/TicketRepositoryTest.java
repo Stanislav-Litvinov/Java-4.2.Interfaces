3b26c9f56db91f4c82812ef206414f0dec49cd89
STANISLAV LITVINOV <91020862+Stanislav-Litvinov@users.noreply.github.com>
2022-02-02 01:52:40 +0300

    Correct tests at TicketRepositoryTest

--- src/test/java/ru/netology/repository/TicketRepositoryTest.java
+++ src/test/java/ru/netology/repository/TicketRepositoryTest.java
@@ -15,12 +15,13 @@ class TicketRepositoryTest {
     private static final String KZN = "Казань";
     private static final String SVO = "Шереметьево";
     private static final String VKO = "Внуково";
-    private static String KGD = "Калининград";
+    private static final String KGD = "Калининград";
 
     static Ticket first = new Ticket(1, 2287, LED, KZN, 133);
     static Ticket second = new Ticket(2, 1299, SVO, KZN, 95);
     static Ticket third = new Ticket(3, 2199, VKO, KZN, 95);
     static Ticket fourth = new Ticket(4, 7099, KGD, KZN, 149);
+
 
     @BeforeAll
     static void setUp() {
@@ -32,22 +33,22 @@ class TicketRepositoryTest {
     @Test
     public void shouldFindAll() {
         Ticket[] expected = {first, second, third};
-        Ticket[] actual = repo.getAll();
-        assertArrayEquals(expected, actual);
-    }
-
-    @Test
-    public void shouldRemoveById() {
-        repo.removeById(2);
-        Ticket[] expected = {first, third};
-        Ticket[] actual = repo.getAll();
-        assertArrayEquals(expected, actual);
+        assertArrayEquals(expected, repo.getAll());
     }
 
     @Test
     public void shouldSave() {
         repo.save(fourth);
         Ticket[] expected = {first, second, third, fourth};
+        Ticket[] actual = repo.getAll();
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldRemoveById() {
+        repo.save(fourth);
+        repo.removeById(4);
+        Ticket[] expected = {first, second, third};
         Ticket[] actual = repo.getAll();
         assertArrayEquals(expected, actual);
     }
